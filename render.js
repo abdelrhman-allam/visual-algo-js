a79ca3509f68450dd165c2649a3c3c7f09f78288
Abdelrhman Allam <abdelrhman.allam@gmail.com>
2020-06-30 22:39:46 +0200

    Merge Sort

--- render.js
+++ render.js
@@ -4,74 +4,127 @@
     canvas.width = 256 * 5;
     canvas.height = 256 * 5;
 
-    var Board = function (row, col, size) {
+    var Board = function (row, col, size, time) {
         this.col = col;
         this.row = row;
         this.size = size;
         this.step = 0;
         canvas.width = col * size;
         canvas.height = row * size;
+        this.time = time;
 
-        this.random = function random(){
-            
+        this.random = function random() {
+
             const r = Math.floor(Math.random() * 255);
             const g = Math.floor(Math.random() * 255);
             const b = Math.floor(Math.random() * 255);
             this.rgba = "rgba(" + r + "," + g + "," + b; //rgba(135,26,31,0.4286748831988356)
-            
+
             this.board = new Array(row * col);
             for (var j = 0; j < this.board.length; j++) {
                 this.board[j] = parseFloat(Math.random()).toFixed(2);
             }
-            
+
             this.render();
         }
-        
-        function sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
+
+        function sleep() {
+            return new Promise(resolve => setTimeout(resolve, this.time));
         }
 
-        this.start = async function start(time) {
+        this.start = async function start() {
             for (var i = 0; i < this.board.length; i++) {
-              for (var j = 0; j < this.board.length - i - 1; j++) {
-                  if(this.board[j+1] < this.board[j]){
-                    const tmp = parseFloat(this.board[j+1]);
-                    this.board[j+1] = parseFloat(this.board[j]);
-                    this.board[j] = tmp;
-                  }
-                  await this.drawAndWait(time);
-              }
+                for (var j = 0; j < this.board.length - i - 1; j++) {
+                    if (this.board[j + 1] < this.board[j]) {
+                        const tmp = parseFloat(this.board[j + 1]);
+                        this.board[j + 1] = parseFloat(this.board[j]);
+                        this.board[j] = tmp;
+                    }
+                    await this.drawAndWait();
+                }
             }
         };
 
-        this.merge = function(l, r){
+        this.merge =  async function (l, r) {
+            const a = new Array(l.length + r.length);
+      
+            let k = 0
+            let i = 0
+            let j = 0
+            while (i < l.length && j < r.length) {
+                if (l[i] < r[j]) {
+                    a[k] = l[i]
+                    i = i + 1
+                } else {
+                    a[k] = r[j]
+                    j = j + 1
+                }
+                k = k + 1
+            }
 
+            while (i < l.length) {
+                a[k] = l[i]
+                i = i + 1
+                k = k + 1
+            }
+
+            while (j < r.length) {
+                a[k] = r[j]
+                j = j + 1
+                k = k + 1
+            }
+            
+    
+            return a
         }
-        this.mergeSort = async function(time){
-
+        
+        this._mergeSort = async function (array, n) {
+        
+            const ln = array.length
+            if (ln < 2) {
+                return array
+            }
+            let mid = Math.ceil(ln / 2)
+            
+            let l = array.slice(0, mid)
+            let r = array.slice(mid , ln)
+            
+            l = await this._mergeSort(l, n)
+            r = await this._mergeSort(r,n+mid)
+            a = await this.merge(l, r)
+            
+            for(let m = 0; m < a.length; m++){
+               this.board[n] = a[m]
+               n++
+            }
+            await this.drawAndWait();
+            return a;
+        }
+        this.mergeSort = async function () {
+            await this._mergeSort(this.board, 0, 0);
         }
 
         this.render = function () {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             const board = this.board;
-            
+
             for (let i = 0; i < board.length; i += 1) {
-                const x = this.size * (i%this.col);
+                const x = this.size * (i % this.col);
                 const y = this.size * Math.floor(i / this.row);
                 ctx.beginPath();
                 ctx.rect(x, y, this.size, this.size);
                 ctx.fillStyle = this.rgba + "," + board[i] + ")";
                 ctx.fill();
-                ctx.font = "8px";
+                ctx.font = "6px";
                 ctx.fillStyle = "rgba(255,255,255,1)";
-				ctx.fillText((parseFloat(board[i])*100).toFixed(0).toString(),  x+(size/2),y + (size/2));
+                ctx.fillText((parseFloat(board[i]) * 100).toFixed(0).toString(), x  , y + (size / 2));
             }
         }
 
 
-        this.drawAndWait = async function drawAndWait(time) {
-            this.render(time)
-            await sleep(time);
+        this.drawAndWait = async function drawAndWait() {
+            this.render()
+            await sleep();
         };
     };
     window.VisualBoard = Board;
